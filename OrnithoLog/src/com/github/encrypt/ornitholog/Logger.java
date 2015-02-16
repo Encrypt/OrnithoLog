cdd544f27e5a03b49851b84bc8281e6c0bc12160
Patouz <pereiraalmeida@et.esiea.fr>
2015-02-16 19:40:06 +0100

    Ajout de la class LogFactory et de l'enum LogLevel

--- OrnithoLog/src/com/github/encrypt/ornitholog/Logger.java
+++ OrnithoLog/src/com/github/encrypt/ornitholog/Logger.java
@@ -1,5 +1,39 @@
 package com.github.encrypt.ornitholog;
 
+import com.github.encrypt.ornitholog.logtargets.*;
+import java.util.ArrayList;
+
 public class Logger {
 
+	private ArrayList<LogTarget> target;
+	private String confFile;
+	private LogLevel level;
+	private LogFormatter formatter;
+	LogSaveDAO saveDAO;
+	
+	public Logger(){
+		this.target = new ArrayList<LogTarget>();
+	}
+	
+	public void debug(String string){
+		
+	}
+	
+	public void info(String string){
+		
+	}
+	
+	public void error(String string){
+		
+	}
+	
+	public void addTarget(LogTarget target){
+		
+	}
+	
+	public void setLevel(LogLevel level){
+		
+	}
+	
+	
 }
---
+++ OrnithoLog/src/com/github/encrypt/ornitholog/LoggerFactory.java
@@ -0,0 +1,16 @@
+package com.github.encrypt.ornitholog;
+
+import java.util.ArrayList;
+
+public class LoggerFactory extends Logger {
+
+	private ArrayList<Logger> loggers;
+	
+	
+	public Logger getLogger(String string){
+		
+		
+		return null;	
+	}
+	
+}
