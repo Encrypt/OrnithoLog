eadef20d98a3d1e4e84cddceb9e2c94f49a72aa8
Yann Privé <prive@et.esiea.fr>
2015-02-25 00:12:07 +0100

    Starting the method to read the properties file

--- OrnithoLog/src/com/github/encrypt/ornitholog/LoggerFactory.java
+++ OrnithoLog/src/com/github/encrypt/ornitholog/LoggerFactory.java
@@ -1,16 +1,17 @@
 package com.github.encrypt.ornitholog;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 
-public class LoggerFactory extends Logger {
+public class LoggerFactory {
 
-	private ArrayList<Logger> loggers;
+	private static ArrayList<Logger> loggers;
 	
-	public LoggerFactory(String className) {
-		super(className);
-	}
-	
-	public Logger getLogger(String maClasse){
+	// Gets a logger, if we give it a name
+	public static Logger getLogger(String maClasse) {
 		for(Logger logger : loggers){
 			
 			if(logger.className.equals(maClasse)){
@@ -19,6 +20,42 @@ public class LoggerFactory extends Logger {
 		}
 		Logger logger = new Logger(maClasse);
 		return logger;	
+	}
+	
+	// Inits the loggers with the values of the properties file
+	// Logger format example: fr.esiea.maclasse:cible2.path = "truc"
+	public static void initLoggers() {
+		
+		String readLine;
+		String[] tokens = new String[2];
+		String[] attributes = new String[2];
+		
+		// Reads the properties file
+		try {
+			BufferedReader br = new BufferedReader(new FileReader("properties"));
+	
+			while ((readLine = br.readLine()) != null) {
+				tokens = readLine.split(":");
+				
+				// Gets the associated logger
+				Logger logger = getLogger(tokens[0]);
+				
+				// Gets the attributes to set
+				attributes = readLine.split("=");
+				
+				// TODO
+				
+			}
+			
+			br.close();
+	
+		}
+		catch(FileNotFoundException e) {
+			System.err.println("Impossible d'accéder au fichier properties");
+		}
+		catch(IOException e) {
+			System.err.println("Erreur lors de la lecture du fichier properties");
+		}
 	}
 	
 }
